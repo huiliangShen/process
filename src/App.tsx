c3d9d7c530a33dc5737db658c84fa270423c5724
banruo <shl@dataqin.com>
2021-07-22 21:06:42 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Process from './components'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Process />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './index.less'
+
+const Process = () => {
+    return <div className={'process-wrap'}>
+        <div>
+            <div className="process-node">
+                <div className="process-node-header">asdqw</div>
+                <div className="process-node-content">
+                    hahah
+                </div>
+            </div>
+            <div className="process-add-btn">
+                <button type={'button'}>+</button>
+            </div>
+        </div>
+        <div>
+            <div className="conditions">
+                <div className="conditions-add">
+                    <a className={'conditions-add-btn'}>添加条件</a>
+                </div>
+                <div className="conditions-columns">
+                    <div className="process-node">
+                        <div className="process-node-header">asdqw</div>
+                        <div className="process-node-content">
+                            hahah
+                        </div>
+                    </div>
+                    <div className="process-add-btn">
+                        <button type={'button'}>+</button>
+                    </div>
+                </div>
+                <div className="conditions-columns">
+                    <div className="process-node">
+                        <div className="process-node-header">asdqw</div>
+                        <div className="process-node-content">
+                            hahah
+                        </div>
+                    </div>
+                    <div className="process-add-btn">
+                        <button type={'button'}>+</button>
+                    </div>
+                </div>
+                <div className="conditions-columns">
+                    <div className="process-node">
+                        <div className="process-node-header">asdqw</div>
+                        <div className="process-node-content">
+                            hahah
+                        </div>
+                    </div>
+                    <div className="process-add-btn">
+                        <button type={'button'}>+</button>
+                    </div>
+                </div>
+                <div className="conditions-columns">
+                    <div className="process-node">
+                        <div className="process-node-header">asdqw</div>
+                        <div className="process-node-content">
+                            hahah
+                        </div>
+                    </div>
+                    <div className="process-add-btn">
+                        <button type={'button'}>+</button>
+                    </div>
+                </div>
+            </div>
+            <div className="process-add-btn">
+                <button type={'button'}>+</button>
+            </div>
+        </div>
+        <div className="process-end-node">
+            <div className="process-end-node-circle"/>
+            <div className="process-end-node-text">结束</div>
+        </div>
+    </div>
+}
+
+export default Process
